893971a3f007fa39e2277a2cd7711355af85e096
Elton Oliveira <85809824+eosilva10@users.noreply.github.com>
2022-05-27 19:37:51 -0300

    Adicionando outra rota

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send({ message: 'OLÁ MUNDO, ESTPU FUNCIONANDO!'});
+    res.status(200).send({ message: 'OLÁ MUNDO, ESTOU FUNCIONANDO!'});
+})
+
+app.get('/ola', (req, res) => {
+    res.status(200).send({ message: 'SOU A ROTA OLÁ!'});
 })
 
 app.listen(3001, ()=> {
